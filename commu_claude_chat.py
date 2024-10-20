acddecba8a9c74000feac57950ed60309ff5a678
Shuya Hashitani <p9fk1v0w@s.okayama-u.ac.jp>
2024-10-20 16:40:08 +0900

    prompt ajustment
    Don't let () appear in the conversation

--- commu_claude_chat.py
+++ commu_claude_chat.py
@@ -53,6 +53,7 @@ class CommuClaudeChat:
         self.system_prompt += f'これから{",".join(names)}の発言を生成してください．'
         self.system_prompt += f'名前は行頭に[]で表記してください．'
         self.system_prompt += f'それに続き発言者の感情を(joy)(question)(interest)(surprise)から選んで答えてください．'
+        self.system_prompt += "感情を表すとき以外に()は使用しないでください"
         self.system_prompt += f'各人物の発言はできるだけ200字以内にしてください．'
         self.system_prompt += f'文章の終わりは必ず句読点で終わるようにしてください．'
 
